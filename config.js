059f05d5099e95d5ec20d9ddcaa44115e24b9307
hbahn <hb.ahn@sk.com>
2017-07-27 15:31:29 +0900

    version rollback and add http RPC request

--- config.js
+++ config.js
@@ -6,9 +6,10 @@ module.exports = {
 
     TREHost : '223.39.121.158',
     TREPort : '1883',
+    TREHttpPort : '9000',
 
     // 20-digits Device Access Token given by manufacturer
-    userName : '22223333444455556666', // Please input your access token
+    userName : '11112222333344445555', // Please input your access token
     //passWord : '',
 
     tremoteyeClientId : function() {
@@ -16,8 +17,12 @@ module.exports = {
         return PRE_FIX + Math.random().toString(16).substr(2, 8);
     },
 
-    sendingTopic : 'v1/sensors/me/tre',
-    rpcReqTopic : 'v1/sensors/me/rpc/request/+',
-    rpcResTopic : 'v1/sensors/me/rpc/response/',
-    updateInterval : 2000
+    sendingTopic : 'v1/devices/me/telemetry',
+    rpcReqTopic : 'v1/devices/me/rpc/request/+',
+    rpcResTopic : 'v1/devices/me/rpc/response/',
+    updateInterval : 2000,
+
+    jwt_token : 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJ1c2VySWQiOiJjYzNjZTEwMC05ZmNlLTExZTYtODA4MC04MDgwODA4MDgwODAiLCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiY2JhNDRhODAtOWZjZS0xMWU2LTgwODAtODA4MDgwODA4MDgwIiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCIsImlzcyI6InRoaW5nc2JvYXJkLmlvIiwiaWF0IjoxNTAxMTM1ODYxLCJleHAiOjE1MDEyMjU4NjF9.LdPmDC_w3N_D3ec449pmKobFsUynOx9Elm84lBiEyLVu_xCNeRt8hqxT823B0yLfLVVhvjPFNoYqcdd86lwPjQ',
+
+    sensorId : '257fb400-7291-11e7-b3c3-5d289f8f8d18'
 }
