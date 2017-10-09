ef2463046fbc580bd574f8629c6b0205ddcb6deb
hbahn <hb.ahn@sk.com>
2017-10-09 15:31:11 +0900

    dd

--- config.js
+++ config.js
@@ -21,8 +21,8 @@ module.exports = {
     rpcReqTopic : 'v1/sensors/me/rpc/request/+',
     rpcResTopic : 'v1/sensors/me/rpc/response/',
     rpcRstTopic : 'v1/sensors/me/rpc/result/',
-    updateInterval : 2000,
-    microTripCnt : 20,
+    updateInterval : 3000,
+    microTripCnt : 50,
 
     jwt_token : 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJ1c2VySWQiOiJjYzNjZTEwMC05ZmNlLTExZTYtODA4MC04MDgwODA4MDgwODAiLCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiY2JhNDRhODAtOWZjZS0xMWU2LTgwODAtODA4MDgwODA4MDgwIiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCIsImlzcyI6InRoaW5nc2JvYXJkLmlvIiwiaWF0IjoxNTAxMTM1ODYxLCJleHAiOjE1MDEyMjU4NjF9.LdPmDC_w3N_D3ec449pmKobFsUynOx9Elm84lBiEyLVu_xCNeRt8hqxT823B0yLfLVVhvjPFNoYqcdd86lwPjQ'
 
